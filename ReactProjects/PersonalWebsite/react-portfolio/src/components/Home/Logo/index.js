23643af5a5177f28a33e405cc0bfb70e2a9cac1a
AnthonyEspinozaCS <aespinoza.cs@gmail.com>
2023-11-06 15:02:01 -0800

    Refactored a lot of bad styles from the tutorial. Used flexbox for responsiveness.

--- ReactProjects/PersonalWebsite/react-portfolio/src/components/Home/Logo/index.js
+++ ReactProjects/PersonalWebsite/react-portfolio/src/components/Home/Logo/index.js
@@ -1,46 +1,56 @@
-import './index.scss';
-import LogoA from '../../../assets/images/Logo-A.png';
-import { useEffect, useRef } from 'react';
-import gsap from 'gsap';
-import Loader from 'react-loaders';
+import './index.scss'
+import LogoA from '../../../assets/images/Logo-A.png'
+import { useEffect, useRef } from 'react'
+import gsap from 'gsap'
+import Loader from 'react-loaders'
 
 const Logo = () => {
-    const bgRef = useRef()
-    const outlineLogoRef = useRef()
-    const innerOutlineRef = useRef()
-    const solidLogoRef = useRef()
-    const tl = useRef(gsap.timeline())
+  const bgRef = useRef()
+  const outlineLogoRef = useRef()
+  const innerOutlineRef = useRef()
+  const solidLogoRef = useRef()
+  const tl = useRef(gsap.timeline())
 
-    useEffect(() => {
+  useEffect(() => {
+    tl.current.to(bgRef.current, {
+      duration: 1,
+      opacity: 1,
+    })
 
-        tl.current
-        .to(bgRef.current, {
-            duration: 1,
-            opacity: 1,
-        })
+    gsap.fromTo(
+      solidLogoRef.current,
+      {
+        opacity: 0,
+      },
+      {
+        opacity: 1,
+        delay: 4,
+        duration: 4,
+      }
+    )
+  }, [])
 
-        gsap.fromTo(
-            solidLogoRef.current,
-            {
-                opacity: 0,
-            }, {
-                opacity: 1,
-                delay: 4,
-                duration: 4,
-            }
-        )
-    }, [])
-
-    return (
-        <>
-            <div className='logo-container' ref={bgRef}>
-                <img ref={solidLogoRef} className="solid-logo" src={LogoA} alt="S"/>
-                <svg version="1.0" xmlns="http://www.w3.org/2000/svg"
-                width="550.000000pt" height="882.000000pt" viewBox="0 0 550.000000 882.000000"
-                preserveAspectRatio="xMidYMid meet">
-                <g className="svg-container" transform="translate(0.000000,882.000000) scale(0.100000,-0.100000)"
-                fill="#000000" stroke="#9c0c02">
-                <path ref={outlineLogoRef} d="M1370 8807 c0 -1 -306 -1532 -680 -3402 l-680 -3400 0 -988 c0 -544
+  return (
+    <>
+      <div className="logo-container" ref={bgRef}>
+        <img ref={solidLogoRef} className="solid-logo" src={LogoA} alt="S" />
+        <svg
+          version="1.0"
+          xmlns="http://www.w3.org/2000/svg"
+          width="550.000000pt"
+          height="882.000000pt"
+          viewBox="0 0 550.000000 882.000000"
+          preserveAspectRatio="xMidYMid meet"
+        >
+          <g
+            className="svg-container"
+            transform="translate(0.000000,882.000000) scale(0.100000,-0.100000)"
+            fill="#000000"
+            stroke="#9c0c02"
+          >
+            <path
+              ref={outlineLogoRef}
+              d="M1370 8807 c0 -1 -306 -1532 -680 -3402 l-680 -3400 0 -988 c0 -544
                 -2 -993 -5 -998 -4 -5 260 -9 687 -9 l694 0 363 1568 c200 862 371 1595 378
                 1630 l15 62 534 0 c330 0 534 -4 534 -9 0 -19 431 -3245 434 -3248 1 -2 419
                 -2 928 -1 l925 3 -783 4395 c-431 2417 -783 4396 -783 4398 -1 3 -2561 3
@@ -52,17 +62,21 @@ const Logo = () => {
                 5747 1496 7325 102 495 207 1010 235 1145 27 135 52 253 55 263 5 16 64 17
                 1055 17 757 0 1049 -3 1049 -11z m-277 -5531 c7 -28 617 -3205 617 -3213 0 -3
                 -135 -5 -300 -5 l-300 0 -5 23 c-6 29 -425 3181 -425 3201 0 14 25 16 204 16
-                203 0 204 0 209 -22z"/>
-                <path ref={innerOutlineRef} d="M2476 8118 c-12 -73 -298 -2999 -294 -3004 3 -2 319 -3 704 -2 l699
+                203 0 204 0 209 -22z"
+            />
+            <path
+              ref={innerOutlineRef}
+              d="M2476 8118 c-12 -73 -298 -2999 -294 -3004 3 -2 319 -3 704 -2 l699
                 3 -180 1515 -181 1515 -371 3 -372 2 -5 -32z m729 10 c6 -17 355 -2944 355
                 -2980 l0 -28 -680 0 c-641 0 -680 1 -680 18 0 27 291 2976 295 2990 3 9 83 12
-                355 12 271 0 352 -3 355 -12z"/>
-                </g>
-                </svg>
-            </div>
-            <Loader type="pacman" />
-        </>
-    )
+                355 12 271 0 352 -3 355 -12z"
+            />
+          </g>
+        </svg>
+      </div>
+      <Loader type="pacman" />
+    </>
+  )
 }
 
 export default Logo
--- ReactProjects/PersonalWebsite/react-portfolio/src/components/Projects/index.js
+++ ReactProjects/PersonalWebsite/react-portfolio/src/components/Projects/index.js
@@ -1,20 +1,9 @@
 import React, { useState, useEffect } from 'react'
-import {
-  Box,
-  ImageList,
-  ImageListItem,
-  imageListItemClasses,
-  ImageListItemBar,
-  imageListItemBarClasses,
-} from '@mui/material'
-import { createTheme, ThemeProvider, styled } from '@mui/material/styles'
-import useMediaQuery from '@mui/material/useMediaQuery'
-import { sizing } from '@mui/system'
+import Loader from 'react-loaders'
 import AnimatedLetters from '../AnimatedLetters'
-import img from '../../assets/images/image1.png'
+import img from '../../assets/images/game.png'
 import img2 from '../../assets/images/b2new.png'
 import img3 from '../../assets/images/keeping.png'
-import img4 from '../../assets/images/full.jpg'
 import './index.scss'
 
 export default function Projects() {
@@ -26,103 +15,49 @@ export default function Projects() {
     }, 3000)
   }, [])
 
-  const theme = createTheme({
-    breakpoints: {
-      values: {
-        mobile: 0,
-        bigMobile: 350,
-        tablet: 650,
-        desktop: 900,
-      },
-    },
-  })
-
-  const mediaQuery = useMediaQuery(theme.breakpoints.up('tablet'))
-
   const projects = [
     {
       title: 'Endless Runner Game',
       image: img,
       url: 'https://endlessjsgame.netlify.app/',
-      cols: 1,
-      rows: 1,
     },
     {
       title: 'Back 2 New BBQ',
       image: img2,
       url: 'https://back2new.netlify.app/',
-      cols: 1,
-      rows: 1,
     },
     {
       title: 'Keeping Care',
       image: img3,
       url: 'https://keepingcare.ca/',
-      cols: 1,
-      rows: 1,
     },
   ]
 
-  const CustomizedILT = styled(ImageListItemBar)(({ theme }) => ({
-    [`& .${imageListItemBarClasses.title}`]: {
-      color: '#9c0c02',
-      fontSize: '15px',
-      '&:hover': {
-        color: '#fff',
-      },
-    },
-  }))
-
   return (
-    <div className="container project-page">
-      <div className="text-zone">
-        <h1>
-          <AnimatedLetters
-            letterClass={letterClass}
-            strArray={['P', 'r', 'o', 'j', 'e', 'c', 't', 's']}
-            idx={16}
-          />
-        </h1>
-        <div className="project-area">
-          <ThemeProvider theme={theme}>
-            <ImageList
-              sx={{
-                width: '100%',
-                minHeight: '600px',
-                padding: '2rem 0',
-                margin: '0',
-                overflow: 'visible',
-              }}
-              cols={mediaQuery ? 3 : 2}
-              rowHeight={mediaQuery ? 250 : 300}
-              gap={'2rem'}
-            >
-              {projects.map((item) => (
-                <a target="_blank" rel="noreferrer" href={item.url}>
-                  <ImageListItem
-                    key={item.title}
-                    sx={{ boxShadow: '0px 0px 3px white' }}
-                  >
-                    <img
-                      src={`${item.image}`}
-                      alt={item.title}
-                      loading="lazy"
-                    />
-                    <CustomizedILT
-                      sx={{
-                        background: 'rgba(0,0,0,0.7)',
-                        maxHeight: '25%',
-                      }}
-                      position="bottom"
-                      title={item.title}
-                    />
-                  </ImageListItem>
-                </a>
+    <>
+      <div className="container project-page">
+        <div className="text-zone">
+          <h1 className="project-header">
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={['P', 'r', 'o', 'j', 'e', 'c', 't', 's']}
+              idx={16}
+            />
+          </h1>
+          <div className="project-area">
+            {projects &&
+              projects.map((project, index) => (
+                <div key={index} className="project">
+                  <a href={project.url}>
+                    <img src={project.image} alt="Screenshot of the website." />
+                    <h2>{project.title}</h2>
+                  </a>
+                </div>
               ))}
-            </ImageList>
-          </ThemeProvider>
+          </div>
         </div>
       </div>
-    </div>
+      <Loader type="pacman" />
+    </>
   )
 }
